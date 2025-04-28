ae27788b8a11c712cb6f544cecb8710a15c62f5c
AddmHariz <adamhariz1601@gmail.com>
2025-04-28 16:13:55 +0800

    Update custom-start.bat

--- custom-start.bat
+++ custom-start.bat
@@ -3,7 +3,7 @@ setlocal
 
 echo.
 echo Starting server...
-java -Xmx10G -Xms10G -jar fabric-server-mc.1.21.4-loader.0.16.10-launcher.1.0.1.jar
+java -Xmx12G -Xms12G -jar fabric-server-mc.1.21.4-loader.0.16.10-launcher.1.0.1.jar
 echo Server process has exited.
 
 echo Verifying shutdown and session safety...
